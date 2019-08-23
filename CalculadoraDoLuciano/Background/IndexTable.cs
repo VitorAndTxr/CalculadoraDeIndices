18a56a7b630c434c2923679089c412cb3b43eb1b
Vitor Andrade <vgabrielteixeira@gmail.com>
2019-08-23 03:13:07 -0300

    Criação do objeto Tabela Inbdexada

--- CalculadoraDoLuciano/Background/IndexTable.cs
+++ CalculadoraDoLuciano/Background/IndexTable.cs
@@ -10,16 +10,31 @@ namespace CalculadoraDoLuciano.Background
     {
         public List<string> cities { get; set; }
         public List<string> index { get; set; }
-        public int[][] citiesIndex { get; set; }
+        public List<float[]> citiesIndex { get; set; }
 
 
         public IndexTable(CSV table)
         {
-            for (var i=0; i<table.)
-            {
+            int i;
+            var buffer = table.GetLineValues(0);
 
+            for(i = 1 ; i < buffer.Length;i++)
+            {
+                index.Add(buffer[i]);
             }
 
+            i=1;
+            while(table.GetLineValues(i)!=null)
+            {
+              buffer = table.GetLineValues(i);
+              cities.Add(buffer[0]);
+
+              for(int j=1;i<buffer.Length;j++)
+              {
+                  float[] aux = new float[buffer.Length-1];
+                  aux[j-1] = float.Parse(buffer[j]); 
+              }
+            }
         }
     }
 }
